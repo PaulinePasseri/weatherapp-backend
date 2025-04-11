8bace09528b7b84834aab7825e3d54c326b51dd7
paulinepasseri <paulinecpasseri@gmail.com>
2025-04-11 16:20:09 +0200

    retrait fichier inutile

--- urls.js
+++
@@ -1,7 +0,0 @@
-// Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://weatherapp-backend-nine-olive.vercel.app';
-
-// Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://weatherapp-frontend-kappa-pearl.vercel.app';
-
-module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
